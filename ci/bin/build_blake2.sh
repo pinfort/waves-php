85dcad6cc9a9f3c207c4f24e235d53404390e639
pinfort <ptg@nijitei.com>
2019-03-16 02:45:09 +0900

    sh files change

--- ci/bin/build_blake2.sh
+++ ci/bin/build_blake2.sh
@@ -1,7 +1,6 @@
 # !/bin/sh
 
-git clone git@github.com:strawbrary/php-blake2.git
-cd php-blake2
+git clone git@github.com:strawbrary/php-blake2.git && cd php-blake2
 phpize
 ./configure --enable-blake2
 make && sudo make install
