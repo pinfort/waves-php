6ea7111ba6b6bc35863e1b60512afa6f6ecc0558
pinfort <ptg@nijitei.com>
2019-03-16 00:42:32 +0900

    make mock response config

---
+++ src/Config/values/fakeResponses.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'GET' => [
+        '/addresses' => [
+            "3PM1fmuMNZPntnHQLBaC8bKpJXUjuEmCRx5"
+        ],
+        '/addresses/seq/0/3' => [
+            "3NAotE3D4S6beBVBKi2V2JS3W3A4KAZVLzV",
+            "3MzemqBzJ9h844PparHU1EzGC5SQmtH5pNp",
+            "3Mstq4YsYUUGrZbXfxYVYKZtHusaVupiKkU"
+        ],
+    ],
+    'POST' => [
+
+    ]
+];
