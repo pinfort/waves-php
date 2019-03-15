0f532a326b0d254b74ac4b6958dea2ecccad70cd
pinfort <ptg@nijitei.com>
2019-03-16 02:37:59 +0900

    travis ci用ファイル追加

---
+++ ci/bin/build_blake2.sh
@@ -0,0 +1,7 @@
+# !/bin/sh
+
+git clone git@github.com:strawbrary/php-blake2.git
+cd php-blake2
+phpize
+./configure --enable-blake2
+make && sudo make install
---
+++ ci/bin/build_curve25519.sh
@@ -0,0 +1,8 @@
+# !/bin/sh
+
+git clone git@github.com:mgp25/curve25519-php.git
+cd curve25519-php
+phpize
+./configure --enable-curve25519
+make
+sudo make install
