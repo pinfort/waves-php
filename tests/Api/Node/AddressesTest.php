b9f9011e8589f7b90e36e27597a2eb177b5714c4
pinfort <ptg@nijitei.com>
2019-03-16 18:22:54 +0900

    test exception

--- tests/Api/Node/AddressesTest.php
+++ tests/Api/Node/AddressesTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use Pinfort\wavesPHP\Api\Node\Addresses;
 use Pinfort\wavesPHP\Config\Config;
 use Pinfort\wavesPHP\Tests\Http\Api;
+use InvalidArgumentException;
 
 class AddressesTest extends TestCase
 {
@@ -32,8 +33,17 @@ class AddressesTest extends TestCase
         $actual = $addresses->fetchAddresses(0, 3);
         $this->assertEquals(Config::get('fakeResponses.GET')['/addresses/seq/0/3'], $actual);
 
-        $this->expectException(\InvalidArgumentException::class);
-        $addresses->fetchAddresses(1);
+        try {
+            $addresses->fetchAddresses(1);
+        } catch (InvalidArgumentException $e) {
+            $this->assertEquals('FromIndex and toIndex wants each other. must not be null.', $e->getMessage());
+        }
+
+        try {
+            $addresses->fetchAddresses(null, 1);
+        } catch (InvalidArgumentException $e) {
+            $this->assertEquals('FromIndex and toIndex wants each other. must not be null.', $e->getMessage());
+        }
     }
 
     /**
