c615571c457f0d5fa5668ddcfe5e886be116501c
pinfort <ptg@nijitei.com>
2019-03-27 13:04:55 +0900

    add api test

---
+++ tests/Http/ApiTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pinfo
+ * Date: 2019/03/27
+ * Time: 12:47
+ */
+
+namespace Pinfort\wavesPHP\Tests\Http;
+
+use Pinfort\wavesPHP\Http\Api;
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+
+}
