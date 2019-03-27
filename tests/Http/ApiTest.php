d22173a8863fc44106048e3f880c96e28d068045
pinfort <ptg@nijitei.com>
2019-03-27 13:17:34 +0900

    add test

--- tests/Http/ApiTest.php
+++ tests/Http/ApiTest.php
@@ -34,5 +34,12 @@ class ApiTest extends TestCase
         $api = new Api();
         $res = $api->get('/pinfort/813fe86b0337495d0399fae4289a474e/raw/c0ccf4d823f69fda3d09a0f29b0c45b83a1eaeff/example.json', 'https://gist.githubusercontent.com');
         $this->assertEquals([ "name" => "John", "age" => 30, "car" => null ], $res);
+
+        Config::reset();
+        Config::set('general.ONLINE', true);
+        Config::set('node.NODE', 'https://gist.githubusercontent.com');
+        $api = new Api();
+        $res = $api->get('pinfort/813fe86b0337495d0399fae4289a474e/raw/c0ccf4d823f69fda3d09a0f29b0c45b83a1eaeff/example.json');
+        $this->assertEquals([ "name" => "John", "age" => 30, "car" => null ], $res);
     }
 }
