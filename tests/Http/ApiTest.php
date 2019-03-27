5bf91ea0382f5dcf5e9f0335aa84c486bb1051b6
pinfort <ptg@nijitei.com>
2019-03-27 13:06:30 +0900

    fix test

--- tests/Http/ApiTest.php
+++ tests/Http/ApiTest.php
@@ -8,10 +8,31 @@
 
 namespace Pinfort\wavesPHP\Tests\Http;
 
+use Pinfort\wavesPHP\Config\Config;
 use Pinfort\wavesPHP\Http\Api;
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase
 {
 
+    public function testPost()
+    {
+        $this->markTestIncomplete('not implemented');
+    }
+
+    public function testGet()
+    {
+        Config::set('node.NODE', 'https://gist.githubusercontent.com');
+        Config::set('general.ONLINE', true);
+        $api = new Api();
+        $res = $api->get('/pinfort/813fe86b0337495d0399fae4289a474e/raw/c0ccf4d823f69fda3d09a0f29b0c45b83a1eaeff/example.json');
+        $this->assertEquals([ "name" => "John", "age" => 30, "car" => null ], $res);
+
+        Config::reset();
+
+        Config::set('general.ONLINE', true);
+        $api = new Api();
+        $res = $api->get('/pinfort/813fe86b0337495d0399fae4289a474e/raw/c0ccf4d823f69fda3d09a0f29b0c45b83a1eaeff/example.json', 'https://gist.githubusercontent.com');
+        $this->assertEquals([ "name" => "John", "age" => 30, "car" => null ], $res);
+    }
 }
