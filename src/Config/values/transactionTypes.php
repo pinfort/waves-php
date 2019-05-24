5a351d7307a0e98f3b62cc7c2c74114ec1cba29f
pinfort <ptg@nijitei.com>
2019-05-24 18:04:29 +0900

    define transactiontypes config

---
+++ src/Config/values/transactionTypes.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'GENESIS' => 1,
+    'ISSUE' => 3,
+    'TRANSFER' => 4,
+    'REISSUE' => 5,
+    'BURN' => 6,
+    'EXCHANGE' => 7,
+    'LEASE' => 8,
+    'LEASE_CANCEL' => 9,
+    'ALIAS' => 10,
+    'MASS_TRANSFER' => 11,
+    'DATA' => 12,
+    'SET_SCRIPT' => 13,
+    'SPONSORSHIP' => 14,
+    'SET_ASSET_SCRIPT' => 15,
+    'INVOKE_SCRIPT' => 16,
+];
